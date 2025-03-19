f5a082d8bb6facc8ac0a0551ffbfc955f1903322
Victor Mendoza <victormendoza1305@gmail.com>
2025-03-19 14:25:05 +0100

    First commit of Next.js

---
+++ app/page.jsx
@@ -0,0 +1,29 @@
+
+import LikeButton from './like-buttons';
+
+
+function Header({ title }) {
+  return <h1>{title ? title : "Default title"}</h1>
+}
+
+export default function HomePage() {
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship." />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <LikeButton />
+
+    </div>
+  );
+}
